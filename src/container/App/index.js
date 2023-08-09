cbfb8ccb9e341d39a45b4610f9bfab70113c6822
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2023-08-09 09:13:09 +0700

    tích hop redux-redux DevTools

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,17 +1,21 @@
-
-import React, {Component} from "react";
+import React, { Component } from "react";
 import styles from "./styles.js";
 import { withStyles } from "@material-ui/core";
-import {ThemeProvider} from "@material-ui/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import Taskboard from "../Taskboard/index.js";
 import theme from "../../commons/theme/index.js";
+import { Provider } from "react-redux";
+import configureStore from "../../redux/configureStore";
+const store = configureStore();
 class App extends Component {
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <Taskboard></Taskboard>
-            </ThemeProvider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Taskboard></Taskboard>
+        </ThemeProvider>
+      </Provider>
+    );
+  }
 }
 export default withStyles(styles)(App);
--- src/container/Taskboard/index.js
+++ src/container/Taskboard/index.js
@@ -28,54 +28,57 @@ const listTask = [
   },
 ];
 class Taskboard extends Component {
-    state = {
-        open: false
-    };
-    renderBoard() {
-        let xhtml = null;
-        xhtml = (
-        <Grid container spacing={2}>
-            {STATUSES.map((status) => {
-            const taskFiltered = listTask.filter(
-                (task) => task.status === status.value
-            );
-            return (
-                <TaskList key={status.value} tasks={taskFiltered} status={status} />
-            );
-            })}
-        </Grid>
-        );
-        return xhtml;
-    }
-    handleClose = () => {
-        this.setState({
-            open: false
-        });
-    };
-    openForm = () => {
-        this.setState({
-            open: true
-        })
-    }
-    renderForm() {
-        const {open} = this.state;
-        let xhtml = null;
-        xhtml = (
-            <TaskForm open = {open} onClose = {this.handleClose}/>
-        );
-        return xhtml;
-    }
-    render() {
-        const { classes } = this.props;
-        return (
-        <div className={classes.taskBoard}>
-            <Button variant="contained" color="primary" className={classes.button} onClick={this.openForm}>
-            <AddIcon /> Thêm mới công việc
-            </Button>
-            {this.renderBoard()}
-            {this.renderForm()}
-        </div>
-        );
-    }
+  state = {
+    open: false,
+  };
+  renderBoard() {
+    let xhtml = null;
+    xhtml = (
+      <Grid container spacing={2}>
+        {STATUSES.map((status) => {
+          const taskFiltered = listTask.filter(
+            (task) => task.status === status.value
+          );
+          return (
+            <TaskList key={status.value} tasks={taskFiltered} status={status} />
+          );
+        })}
+      </Grid>
+    );
+    return xhtml;
+  }
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+  openForm = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  renderForm() {
+    const { open } = this.state;
+    let xhtml = null;
+    xhtml = <TaskForm open={open} onClose={this.handleClose} />;
+    return xhtml;
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.taskBoard}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={this.openForm}
+        >
+          <AddIcon /> Thêm mới công việc
+        </Button>
+        {this.renderBoard()}
+        {this.renderForm()}
+      </div>
+    );
+  }
 }
 export default withStyles(styles)(Taskboard);
